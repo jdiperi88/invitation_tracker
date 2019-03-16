a5dff9a1c856a958bf75b4f444423d2f6bec2983
jdiperi88 <jdiperi88@yahoo.com>
2019-03-16 08:33:30 -0400

    edited some scripts

--- models/index.js
+++ models/index.js
@@ -3,6 +3,9 @@ const dbName = "invitation_db";
 
 const db = new Sequelize({
   database: dbName,
+  host:
+    process.env.DATABASE_URL ||
+    "postgres://yzrzkssrdczgnh:57b6b2ad617112eb34bd2f454817fc7715d333a1188595219a33879ced636d84@ec2-54-225-95-183.compute-1.amazonaws.com:5432/dvmovnjbhm2uo",
   dialect: "postgres",
   define: {
     underscored: true
