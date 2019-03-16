38dc1ea8ebf01ab0c9e0ebda7cabf7c3ace2084f
jdiperi88 <jdiperi88@yahoo.com>
2019-03-16 09:42:15 -0400

    changed models file

--- models/index.js
+++ models/index.js
@@ -1,26 +1,14 @@
 const Sequelize = require("sequelize");
 const dbName = "invitation_db";
 
-let db;
-if (process.env.DATABASE_URL) {
-  // the application is executed on Heroku ... use the postgres database
-  db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    protocol: "postgres",
-    port: match[4],
-    host: match[3],
-    logging: true //false
-  });
-} else {
-  // the application is executed on the local machine ... use mysql
-  db = new Sequelize({
-    database: dbName,
-    dialect: "postgres",
-    define: {
-      underscored: true
-    }
-  });
-}
+const db = new Sequelize({
+  database: dbName,
+  host: process.env.DATABASE_URL || "localhost",
+  dialect: "postgres",
+  define: {
+    underscored: true
+  }
+});
 
 const Recipient = require("./Recipient")(db, Sequelize);
 const Survey = require("./Survey")(db, Sequelize);
