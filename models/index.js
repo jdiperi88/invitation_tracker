a507f083d1773522c3e8a5a277a6703d571fa991
sdiperi17 <sdiperi17@gmail.com>
2019-03-16 18:24:20 -0400

    created relationship for tables

--- models/index.js
+++ models/index.js
@@ -2,29 +2,32 @@ const Sequelize = require("sequelize");
 const dbName = "invitation_db";
 let db;
 if (process.env.DATABASE_URL) {
-  db = new Sequelize(process.env.DATABASE_URL, {
-    logging: false,
-    dialectOptions: {
-      ssl: true /* for SSL config since Heroku gives you this out of the box */
-    }
-  });
+    db = new Sequelize(process.env.DATABASE_URL, {
+        logging: false,
+        dialectOptions: {
+            ssl: true /* for SSL config since Heroku gives you this out of the box */
+        }
+    });
 } else {
-  db = new Sequelize({
-    database: dbName,
-    dialect: "postgres",
-    define: {
-      underscored: true
-    }
-  });
+    db = new Sequelize({
+        database: dbName,
+        dialect: "postgres",
+        define: {
+            underscored: true
+        }
+    });
 }
 
 const Recipient = require("./Recipient")(db, Sequelize);
 const Survey = require("./Survey")(db, Sequelize);
 const User = require("./User")(db, Sequelize);
 
+Survey.hasMany(Recipient);
+Recipient.belongsTo(Survey);
+
 module.exports = {
-  Recipient,
-  Survey,
-  User,
-  db
+    Recipient,
+    Survey,
+    User,
+    db
 };
