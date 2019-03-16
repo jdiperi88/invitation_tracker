bab44a303a3052f608136df6927b065f24a0ea89
jdiperi88 <jdiperi88@yahoo.com>
2019-03-16 19:28:51 -0400

    fixed name hash map function

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const passport = require("passport");
 const bodyParser = require("body-parser");
 const { db } = require("./models/index");
 //connecting to the hosted mongodb
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -13,10 +12,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //this tells express to use a cookie session, and we set the cookie to expire after 30 days
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
 );
 
 app.use(passport.initialize());
@@ -33,21 +32,21 @@ app.use("/auth", authRoutes);
 app.use(express.static("client/build"));
 
 if (process.env.NODE_ENV == "production") {
-    // Express will serve up production assets
-    //like main.css and main.js
-    app.use(express.static("client/build"));
+  // Express will serve up production assets
+  //like main.css and main.js
+  app.use(express.static("client/build"));
 
-    //express will serve index.js file if it doesnt recognize the route
-    const path = require("path");
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
+  //express will serve index.js file if it doesnt recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const port = process.env.PORT || 3001;
 
 db.sync().then(function() {
-    app.listen(port, function() {
-        console.log(`listening ${port}`);
-    });
+  app.listen(port, function() {
+    console.log(`listening ${port}`);
+  });
 });
--- util/nameHashMap.js
+++ util/nameHashMap.js
@@ -1,7 +1,9 @@
 const name = {
-    "jdiperi88@gmail.com": "Joey DiPeri",
-    "sdiperi17@gmail.com": "Saida DiPeri",
-    "tyronesanderson@yahoo.com": "Tyrone Sanderson"
+  "jdiperi88@gmail.com": "Joey DiPeri",
+  "sdiperi17@gmail.com": "Saida DiPeri",
+  "tyronesanderson@yahoo.com": "Tyrone Sanderson"
 };
 
-module.exports = () => {};
+module.exports = email => {
+  return name[email];
+};
