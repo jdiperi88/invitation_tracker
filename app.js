082154728604fba247359c177efd9ccd51d7a05e
jdiperi88 <jdiperi88@yahoo.com>
2019-03-16 10:02:31 -0400

    uncommented code in app.js

--- app.js
+++ app.js
@@ -29,18 +29,20 @@ const apiRoutes = require("./routes/api");
 
 app.use("/api", apiRoutes);
 app.use("/auth", authRoutes);
-// app.use(express.static("client/build"));
-// if (process.env.NODE_ENV == "production") {
-// Express will serve up production assets
-//like main.css and main.js
+
 app.use(express.static("client/build"));
 
-//express will serve index.js file if it doesnt recognize the route
-// const path = require("path");
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// });
-// }
+if (process.env.NODE_ENV == "production") {
+  // Express will serve up production assets
+  //like main.css and main.js
+  app.use(express.static("client/build"));
+
+  //express will serve index.js file if it doesnt recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 3001;
 
